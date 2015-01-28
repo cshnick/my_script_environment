92e760e7d9042d16d5347bac82ae8e440e72d595
Ilia Ryabokon <luxa.ryabic@gmail.com>
2015-01-29 00:03:57 +0300

    Static method in table

--- python/multiple_table.py
+++ python/multiple_table.py
@@ -4,6 +4,7 @@
 import random
 from color.colorize import Color
 import signal
+import achivement_engine.AeCore as ae
 
 class Ask:
   great = 0
@@ -18,28 +19,31 @@ class Ask:
     print Color('Неплохо:       ').as_yellow() + Color(str(Ask.good)).as_white()
     print Color('Ошибок:        ').as_purple() + Color(str(Ask.total - Ask.great - Ask.good)).as_white()
     
-  def ask(self, r1, r2):
-    ask = raw_input('' + str(r1) + 'x' + str(r2) + '=')
-    if not ask:
+
+  @staticmethod
+  def ask(r1, r2):
+    ans = raw_input('' + str(r1) + 'x' + str(r2) + '=')
+    if not ans:
       Ask.print_stat()
       exit(0)
     right = r1*r2
-    return (ask, right)
+    return ans, right
 
-  def do_ask(self, r1, r2, toplevel=True):
-    (ans, right) = self.ask(r1, r2)
-    Ask.total = Ask.total + 1
+  @staticmethod
+  def do_ask(r1, r2, toplevel=True):
+    (ans, right) = Ask.ask(r1, r2)
+    Ask.total += 1
     if int(right) == int(ans):
       mesge = 'Крутяк!!!!' if toplevel else 'Неплохо, но можно было бы и с первой попытки'
       col = 'green' if toplevel else 'yellow'
       print Color(mesge).as_color(col)
       if toplevel:
-	Ask.great = Ask.great + 1
+        Ask.great += 1
       else:
-	Ask.good = Ask.good + 1
+	    Ask.good += 1
     else:
       print Color('Попробуй еще раз').as_purple()
-      self.do_ask(r1, r2, toplevel=False)
+      Ask.do_ask(r1, r2, toplevel=False)
     
 def ST_handler(signum, frame):
     Ask.print_stat()
@@ -47,9 +51,22 @@ def ST_handler(signum, frame):
   
 def main():
   signal.signal(signal.SIGINT, ST_handler)
+
+  a = ae.EngineImpl()
+  a.begin()
+  dct = dict()
+  dct['a'] = ae.variant('b')
+  dct['b'] = ae.variant('c')
+  dct['e'] = ae.variant('f')
+  cxx_dct = ae.map_sv(dct)
+  cxx_dct['g'] = ae.variant('h')
+  a.addAction(cxx_dct)
+  a.end()
+
   while True:
+    r1 = random.randint(2, 9)
     r2 = random.randint(2, 9)
-    Ask().do_ask(r1, r2, toplevel=True)
+    Ask.do_ask(r1, r2, toplevel=True)
   
 if __name__ == '__main__':
   main()
