7141d76d7adf80e6f7073ddf9c7ec8c62ae4b555
Ilia Ryabokon <luxa.ryabic@gmail.com>
2015-01-29 21:17:21 +0300

    Environment home default paths

--- bash/environment_start_home.sh
+++ bash/environment_start_home.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-yakuake &
+yakuake-session -q -t "build" --hold --workdir /home/ilia/Development/to_sort/test_curl -e 'clear' 1>/dev/null 2>/dev/null
+yakuake-session -q -t "bin" --hold --workdir "/home/ilia/Development/to_sort/build-qml_currency-Desktop_Qt_5_3_GCC_64bit-Debug" -e 'clear' 1>/dev/null 2>/dev/null
+qdbus org.kde.yakuake /yakuake/tabs setTabTitle 0 "Start" 1>/dev/null 2>/dev/null
+qdbus org.kde.yakuake /yakuake/tabs sessionAtTab 2 1>/dev/null 2>/dev/null
 #sleep 10 
 #reload-compositor &
