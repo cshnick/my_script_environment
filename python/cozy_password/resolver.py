7921d41659b354ac62121fd8f52fd037e2e355a1
Ilia Ryabokon <IRyabokon@ncomputing.com>
2016-12-23 17:51:20 +0300

    get and set functionality, password generator

--- python/cozy_password/resolver.py
+++ python/cozy_password/resolver.py
@@ -3,40 +3,119 @@
 import simplejson as json
 import os
 import file_cryptor
-import cStringIO
+from cStringIO import StringIO
+from contextlib import contextmanager
+from password_generator import generate_pass
+from decorator import decorator
 
 class ResolverBase(object):
     pass
+
+#Open 'file' or 'buffer'
+@contextmanager
+def customopen(*args, **kwargs):
+    stream = open(*args) if type is 'file' else StringIO()
+    try:
+        yield stream
+    finally:
+        stream.close()
+
+@decorator
+def dec1(fn, *args, **kwargs):
+    if len(args) is 1:
+        return fn(args[0], **kwargs)
+    return fn(*args, **kwargs)
+
+
+def load_on_demand(method):
+    def deco(self, *args, **kwargs):
+        if not hasattr(self, "__data") or not self.__data:
+            self.load()
+        return method(self, *args, **kwargs)
+    return deco
+
 
 class ScandResolver(ResolverBase):
     __Pairs = "Pairs"
     __Filename = "scand_map.json"
     __Encrypted = "scand_map.json.enc"
     __Dir_path = os.path.dirname(os.path.realpath(__file__))
+    __Filename_path = __Dir_path + '/' + __Filename
+    __Encrypted_path = __Dir_path + '/' + __Encrypted
 
-    def password_for_name(self, name):
+    def __init__(self):
+        #self.restore()
+        self.__data = None
+
+    @load_on_demand
+    def password_for_name(self, name, secondarg=None):
         if "DEBUG" in os.environ:
             print ("Dir path: %s" % ScandResolver.__Dir_path)
-        with open(ScandResolver.__Dir_path + "/" + ScandResolver.__Filename) as scand_map_file:
-            scand_map = json.load(scand_map_file)
-
+        pairs = self.__data[ScandResolver.__Pairs]
         password = None
-        if name in scand_map[ScandResolver.__Pairs]:
-            password = scand_map[ScandResolver.__Pairs][name]
+        if name in pairs:
+            password = pairs[name]
 
         return password
 
-    def save(self):
-        with open(ScandResolver.__Dir_path + "/" + ScandResolver.__Filename) as scand_map_file:
-            with open(ScandResolver.__Dir_path + "/" + ScandResolver.__Encrypted, "w") as scand_encrypted:
+    @load_on_demand
+    def add_password(self, key, password):
+        if "DEBUG" in os.environ:
+            print ("Inserting password")
+
+        if password is None:
+            password = generate_pass()
+
+        self.__data[ScandResolver.__Pairs][key] = password
+        self.save()
+
+    def __save(self, *args, **kwargs):
+        with customopen(*args, **kwargs) as scand_map_file:
+            with open(ScandResolver.__Encrypted_path, "w") as scand_encrypted:
+                value = scand_map_file.getvalue()
+                if kwargs['type'] is 'buffer':
+                    json.dump(self.__data, scand_map_file)
+                    scand_map_file.seek(0)
+
                 file_cryptor.encrypt(in_file=scand_map_file,
                                      out_file=scand_encrypted,
                                      password="Qwerty#0")
 
-    def restore(self):
-        with open(ScandResolver.__Dir_path + "/" + ScandResolver.__Encrypted, "r") as scand_encrypted:
-            with open(ScandResolver.__Dir_path + "/" + ScandResolver.__Filename, "w") as scand_map_file:
+    def __load(self, *args, **kwargs):
+        with open(ScandResolver.__Encrypted_path, "r") as scand_encrypted:
+            with customopen(*args, **kwargs) as scand_map_file:
                 file_cryptor.decrypt(in_file=scand_encrypted,
                                      out_file=scand_map_file,
                                      password="Qwerty#0")
+
+                scand_map_file.seek(0)
+                self.__data = json.load(scand_map_file)
+
+
+    def save(self):
+        self.__save(type='buffer')
+
+    def save_file(self):
+        self.__save(ScandResolver.__Filename_path, 'r', type='file')
+
+    def load(self):
+        self.__load(self.__data, type='buffer')
+        pass
+
+    def load_from_file(self):
+        self.__load(ScandResolver.__Filename_path, 'r+', type='file')
+        pass
+
+    def restore(self):
+        with open(ScandResolver.__Filename_path, "r") as scand_encrypted:
+            with open(ScandResolver.__Encrypted_path, "w") as scand_map_file:
+                file_cryptor.encrypt(in_file=scand_encrypted,
+                                     out_file=scand_map_file,
+                                     password="Qwerty#0")
+
+
+
+
+
+
 
--- python/cozypassword.py
+++ python/cozypassword.py
@@ -1,35 +1,82 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 import clipboard
 from argparse import ArgumentParser
 from cozy_password.resolver import ScandResolver
 from os import environ
+from os.path import basename
+import os
+import __builtin__
+
+class Const (object):
+    Subprsr_name = 'subparser_name'
+    class Restore:
+        Name = 'restore'
+    class Add:
+        Name = "add"
+        Key = "Add.Key"
+        Password  = "Add.Password"
+    class Get:
+        Name = "get"
+        Key = "Get.Key"
 
 class CmdCozyPassword (object):
-    def __init__(self, get_args=None):
-        parser = ArgumentParser("Cmd parser")
-        subparsers = parser.add_subparsers(help="Subparser to fulfill functionality")
-        parser_get = subparsers.add_parser('get', help="Get functionality")
-        parser_get.add_argument('getargs', help='Get arguments', nargs="+")
+    def __init__(self):
+        filename = basename(__file__)
+        parser = ArgumentParser(filename)
 
-        args = parser.parse_args()
+        subparsers = parser.add_subparsers(help="Subparsers", dest=Const.Subprsr_name)
+        parser_get = subparsers.add_parser(Const.Get.Name, help="Get password from key")
+        parser_get.add_argument(Const.Get.Key, help='Get arguments')
 
-        self.__get_args = args.getargs
+        parser_add = subparsers.add_parser(Const.Add.Name, help="Add new key-password pair")
+        parser_add.add_argument('-k', '--key', help='key(human readable)', dest=Const.Add.Key, required=True)
+        parser_add.add_argument('-p', '--password', help='Password to store (generate if empty)', dest=Const.Add.Password)
+
+        parser_restore = subparsers.add_parser(Const.Restore.Name, help="Attempt to restore(debug only)")
+
+        self.__args =  parser.parse_args()
         self.__resolver = ScandResolver()
+        parser.print_help()
+
+        #print(getattr(args, "add_password", None))
+        #print(getattr(args, "add_password1", None))
+
+        #self.__get_args = args.getargs
+        #self.__add_args = args.addargs
+
+    def __process_args(self):
+        print("Self args: %s" % self.__args)
+        #Callback is starting from process_...
+        name  = 'process_'+self.__args.__dict__[Const.Subprsr_name]
+        callback = getattr(self, name)
+        if callback:
+            callback()
 
 
-    def __process_get(self):
-        key = self.__get_args[0]
-        clipboard.copy(self.__resolver.password_for_name(key))
+    def process_get(self):
+        key = getattr(self.__args, Const.Get.Key, None)
+        password = self.__resolver.password_for_name(key)
+        clipboard.copy(password)
 
+    def process_add(self):
+        key = getattr(self.__args, Const.Add.Key, None)
+        password = getattr(self.__args, Const.Add.Password, None)
+
+        self.__resolver.add_password(key=key, password=password)
+        pass
+
+    def process_restore(self):
+        if "DEBUG" in environ:
+            print("Attempting to restore")
+        self.__resolver.restore()
+        if "DEBUG" in environ:
+            print("Restored, no errors detected")
 
     def main(self):
-        self.__resolver.restore()
-
-        if len(self.__get_args):
-            self.__process_get()
-
         #self.__resolver.save()
+        self.__process_args()
 
         if "DEBUG" in environ:
             print("Clipboard content: %s" % clipboard.paste())
