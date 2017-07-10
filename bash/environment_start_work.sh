a31fa66464ce6e876664c9198ef860e58e1ee7a1
Ilia Ryabokon <iryabokon@ncomputing.com>
2017-07-10 18:50:10 +0300

    work changes

--- bash/environment_start_work.sh
+++ bash/environment_start_work.sh
@@ -7,17 +7,19 @@
 #yakuake-session -q -t "bin32" --hold --workdir /home/ilia/Prognoz/P7/DEV8/build32/bin -e 'clear' >/dev/null
 #PROGNOZ
 
-yakuake-session -q -t "sm src" --hold --workdir /home/ilia/Development/sm_service_model -e 'clear' >/dev/null
-yakuake-session -q -t "sm build" --hold --workdir /home/ilia/Development/cmake_sm -e 'clear' >/dev/null
+#yakuake-session -q -t "sm src" --hold --workdir /home/ilia/Development/sm_service_model -e 'clear' >/dev/null
+#yakuake-session -q -t "sm build" --hold --workdir /home/ilia/Development/cmake_sm -e 'clear' >/dev/null
 
-yakuake-session -q -t "nacl" --hold --workdir /home/ilia/Development/nacl/nacl_sdk/pepper_47 -e 'clear' >/dev/null
-yakuake-session -q -t "nacl ports" --hold --workdir /home/ilia/Development/nacl/webports_gclient -e 'clear' >/dev/null
+#yakuake-session -q -t "nacl" --hold --workdir /home/ilia/Development/nacl/nacl_sdk/pepper_47 -e 'clear' >/dev/null
+#yakuake-session -q -t "nacl ports" --hold --workdir /home/ilia/Development/nacl/webports_gclient -e 'clear' >/dev/null
 
-yakuake-session -q -t "POCO Server" --hold --workdir /home/ilia/Development/nacl/nc-ui-prototype/NcConsole/cmake-Poco/bin -e 'clear' >/dev/null
-yakuake-session -q -t "POCO Client" --hold --workdir /home/ilia/Development/nacl/nc-ui-prototype/NcConsole/cmake-Poco/bin -e 'clear' >/dev/null
+#yakuake-session -q -t "POCO Server" --hold --workdir /home/ilia/Development/nacl/nc-ui-prototype/NcConsole/cmake-Poco/bin -e 'clear' >/dev/null
+#yakuake-session -q -t "POCO Client" --hold --workdir /home/ilia/Development/nacl/nc-ui-prototype/NcConsole/cmake-Poco/bin -e 'clear' >/dev/null
 
-qdbus org.kde.yakuake /yakuake/tabs setTabTitle 0 "Start" 1>/dev/null 2>/dev/null
-qdbus org.kde.yakuake /yakuake/tabs sessionAtTab 2 1>/dev/null 2>/dev/null
+#qdbus org.kde.yakuake /yakuake/tabs setTabTitle 0 "Start" 1>/dev/null 2>/dev/null
+#qdbus org.kde.yakuake /yakuake/tabs sessionAtTab 2 1>/dev/null 2>/dev/null
+
+klipper
 
 
 #krusader 1>/dev/null 2>/dev/null &
